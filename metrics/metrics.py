bfaeb391ed53a4a31391c638b33c280409a73964
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-24 17:02:47 +0200

    MT: Added mse.

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -7,16 +7,26 @@ class Metrics:
         self.val_real = np.array(val_real)
 
         # Calculate TP, TN, FP, FN
-        self.TP = np.sum((self.val_curr > 0.5) & (self.val_real > 0.5))
-        self.TN = np.sum((self.val_curr < 0.5) & (self.val_real < 0.5))
-        self.FP = np.sum((self.val_curr > 0.5) & (self.val_real < 0.5))
-        self.FN = np.sum((self.val_curr < 0.5) & (self.val_real > 0.5))
+        self.TP = np.sum((self.val_curr == 1) & (self.val_real == 1))
+        self.TN = np.sum((self.val_curr == 0) & (self.val_real == 0))
+        self.FP = np.sum((self.val_curr == 1) & (self.val_real == 0))
+        self.FN = np.sum((self.val_curr == 0) & (self.val_real == 1))
 
     # Recall (Sensitivity)
     def recall(self) -> float:
         if self.TP + self.FN == 0:
             return 0.0
         return self.TP / (self.TP + self.FN)
+
+    def rmse(self) -> float:
+        if self.TP + self.FN == 0:
+            return 0.0
+        return np.sqrt((self.TP / (self.TP + self.FN)) * (1 - (self.TP / (self.TP + self.FN))) / (self.TP + self.FN))
+
+    def mse(self) -> float:
+        if self.TP + self.FN == 0:
+            return 0.0
+        return (self.TP / (self.TP + self.FN)) * (1 - (self.TP / (self.TP + self.FN))) / (self.TP + self.FN)
 
     # Accuracy
     def accuracy(self) -> float:
@@ -40,13 +50,15 @@ class Metrics:
 # Test class for Metrics
 class MetricsTest:
     def __init__(self):
-        val_curr = [0, 0.9, 0, 0, 0.5, 0.9, 0.9, 0, 0.9, 0.9]  # predicted validation labels
+        val_curr = [0, 1, 0, 0, 1, 1, 1, 0, 1, 1]  # predicted validation labels
         val_real = [0, 1, 0, 0, 1, 1, 1, 0, 1, 1]  # true validation labels
         self.metrics = Metrics(val_curr, val_real)
 
     # Calculate and print accuracy, recall, precision, and f-measure for validation data
     def runTest(self) -> None:
         print("Recall:", round(self.metrics.recall(), 4))
+        print("RMSE:", round(self.metrics.rmse(), 4))
+        print("MSE:", round(self.metrics.rmse(), 4))
         print("Accuracy:", round(self.metrics.accuracy(), 4))
         print("Precision:", round(self.metrics.precision(), 4))
         print("F-measure:", round(self.metrics.f_measure(), 4))
